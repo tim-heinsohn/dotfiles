b235441dc8a7e3f926ebd212310c0b7ebebbc7ac
Tim Heinsohn <smile@friendsoft.de>
2026-05-15 11:26:26 +0200

    fonts, btop, tmuxp

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,7 +4,12 @@ local config = {
     default_prog = { '/bin/zsh', '-l' },
 
     -- font
+    font = wezterm.font_with_fallback {
+        'JetBrainsMono Nerd Font Mono',
+        'Noto Color Emoji',
+    },
     font_size = 10.0,
+    warn_about_missing_glyphs = false,
 
     -- tab bar
     tab_bar_at_bottom = false,
